b8ee81a480fc2ca7a1e27790dc8572afc7a046f5
kubryak <al.lcn@yandex.ru>
2024-03-27 19:22:47 +0500

    fix: fix position modal element

--- js/index.js
+++ js/index.js
@@ -2,9 +2,7 @@
 
 Fancybox.defaults.closeButton = false;
 
-Fancybox.bind("[data-fancybox]", {
-
-});
+Fancybox.bind("[data-fancybox]", {});
 
 // Замена placeholder у input type=tel при наведении
 
@@ -26,7 +24,9 @@ inputElements.forEach(function (inputElement) {
 
 document.addEventListener("DOMContentLoaded", function () {
     const allHome = document.querySelector(".all-home");
-    const modal = document.querySelector(".plan__modal")
+    const modal = document.querySelector(".plan__modal");
+    const parentBlock = document.querySelector(".plan__map-wrapper");
+    console.log(modal.offsetWidth);
 
     allHome.addEventListener("click", function (event) {
         if (event.target.classList.contains("home-area")) {
@@ -36,21 +36,28 @@ document.addEventListener("DOMContentLoaded", function () {
             if (isActive) {
                 // Если элемент уже активен, удаляем класс "active"
                 clickedArea.classList.remove("home-area_active");
-                modal.classList.add("d-none")
+                modal.classList.add("d-none");
             } else {
                 // Если элемент не активен, убираем класс "active" у других элементов и добавляем его к текущему
-                const currentActive = document.querySelector(".home-area.home-area_active");
+                const currentActive = document.querySelector(
+                    ".home-area.home-area_active"
+                );
                 if (currentActive) {
                     currentActive.classList.remove("home-area_active");
-                    modal.classList.add("d-none")
+                    modal.classList.add("d-none");
                 }
                 clickedArea.classList.add("home-area_active");
 
-                const blockRect = clickedArea.getBoundingClientRect();
+                const parentRect = parentBlock.getBoundingClientRect();
+                const elementRect = clickedArea.getBoundingClientRect();
 
-                modal.style.top = (blockRect.top - 450 + "px");
-                modal.style.left = (blockRect.left - 460 + "px");
-                modal.classList.remove("d-none")
+                const relativeX = elementRect.left - parentRect.left;
+                const relativeY = elementRect.top - parentRect.top;
+
+                modal.style.top = relativeY - 230 + "px";
+                modal.style.left = relativeX - 260 + "px";
+
+                modal.classList.remove("d-none");
             }
         }
     });
